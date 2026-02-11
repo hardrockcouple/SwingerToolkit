2c8361b0bc32d7d7676c658a8dfe5c17f8a045c2
hardrockcouple <hardrockcouple.81@gmail.com>
2026-02-11 00:38:57 +0000

    v1

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
-const CONFIG = {
-  DATA_SOURCE: "./clubs.json",
-  LOGO_PATH: "https://raw.githubusercontent.com/hardrockcouple/swtk/main/logo_txt_full_140px.jpg",
+// config.js
+window.CONFIG = {
+  DATA_SOURCE:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4U36guHIuvS7t1Ej5znJ2Ez_-24-o8mN-XNM8KD05RvR33rV2g6SSW1gusF-aV3FFfNHyejPs4Aj1/pub?gid=1230762840&single=true&output=csv",
+  LOGO_PATH: "https://github.com/hardrockcouple/swtk/raw/main/logo_txt_full_140px.jpg",
   SITE_NAME: "SW Clubs Directory",
   PAGE_SIZE: 12
 };
