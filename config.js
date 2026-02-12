ca3d17d5a92d89f68d5122e95a559f56df709673
hardrockcouple <hardrockcouple.81@gmail.com>
2026-02-12 23:28:23 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+// config.js
+window.CONFIG = {
+  // Fonte de dados (GitHub Pages: caminho relativo ao root do repo publicado)
+  DATA_SOURCE: "./clubs.json",
+
+  // Logo
+  LOGO_PATH: "https://raw.githubusercontent.com/hardrockcouple/swtk/main/logo_txt_full_140px.jpg",
+
+  // Nome do site
+  SITE_NAME: "SW Clubs Directory",
+
+  // Paginação (se quiseres usar mais tarde)
+  PAGE_SIZE: 12
+};
